5e10b1fe42dd10241cd207df3f9beba7fd031bff
Norbert Klasen <nklasen@opentext.com>
2023-11-16 10:05:56 +0100

    Add baseIndex parameter
    Allows simulation of 0-indexed and 1-indexed APIs.

--- src/fulfillmentTestServer.js
+++ src/fulfillmentTestServer.js
@@ -6,15 +6,20 @@
 //
 // Usage: node fulfillmentTestServer.js
 
- 
+
 const url = require('node:url');
 const http = require('http')
 const port = 3000
 const { createHash } = require('node:crypto');
 
+const defaultBaseIndex = 1; // 0 or 1 indexed
+const totalCount = 5; // max number of results to return
+const maxPageSize = totalCount; // number of retults to return if no page size is specified
+
+var requestsTotalCount = 1; // counter of received HTTP requests
 
 const requestHandler = (request, response) => {
-//  console.log(request.url)
+  //  console.log(request.url)
 
 
   if (request.method == 'POST') {
@@ -27,20 +32,20 @@ const requestHandler = (request, response) => {
 
   request.on('end', function () {
     var u = new URL(request.url, `http://${request.headers.host}`);
-    console.log(new Date());	  
+    console.log(new Date());
     console.log("Incoming request #" + requestsTotalCount + ": " + request.method + ' on ' + request.url);
     //console.log(u);
     console.log("Request headers: " + JSON.stringify(request.headers));
     if (body) {
-	    console.log("Request body:", body);
+      console.log("Request body:", body);
     }
 
     if (request.headers['content-type'] && request.headers['content-type'].indexOf('xml') > 0) {
       response.setHeader('content-type', request.headers['content-type']);
       response.end(buildSoapContent());
     } else {
-       response.setHeader('content-type', 'application/json');
-       response.end(buildJsonContent(u, 5, requestsTotalCount));
+      response.setHeader('content-type', 'application/json');
+      response.end(buildJsonContent(u, requestsTotalCount));
     }
     console.log("Response headers: " + JSON.stringify(response.getHeaders()));
     requestsTotalCount++;
@@ -58,35 +63,39 @@ server.listen(port, (err) => {
   console.log(`server is listening on ${port}`)
 })
 
-var buildSoapContent = function() {
+var buildSoapContent = function () {
   var soap = '<?xml version="1.0" ?>\n';
-	soap += '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">\n';
-	soap += '	<soapenv:Body xmlns:m="http://www.myservice.com/incident">\n';
-	soap += '		<m:insertResponse>\n';
-	soap += '			<ticketNumber>' + requestsTotalCount + '</ticketNumber>\n';
-	soap += '		</m:insertResponse>\n';
-	soap += '	</soapenv:Body>\n';
+  soap += '<soapenv:Envelope xmlns:soapenv="http://schemas.xmlsoap.org/soap/envelope/">\n';
+  soap += '	<soapenv:Body xmlns:m="http://www.myservice.com/incident">\n';
+  soap += '		<m:insertResponse>\n';
+  soap += '			<ticketNumber>' + requestsTotalCount + '</ticketNumber>\n';
+  soap += '		</m:insertResponse>\n';
+  soap += '	</soapenv:Body>\n';
   soap += '</soapenv:Envelope>';
   return soap;
 }
 
 
-var buildJsonContent = function(u, totalCount, requestsTotalCount) {
-	const firstIndex = Number(u.searchParams.get('startIndex'));
-	const pageSize = Number(u.searchParams.get('count'));
-	const o = {};
-	o.totalCount = 5;
-	o.request = requestsTotalCount;
-	o.firstIndex = firstIndex;
-	o.pageSize = pageSize;
+var buildJsonContent = function (u, requestsTotalCount) {
+  const baseIndex = u.searchParams.has('baseIndex') ? Number(u.searchParams.get('baseIndex')) : defaultBaseIndex;
+  const startIndex = Number(u.searchParams.get('startIndex'));
+  const pageSize = u.searchParams.has('count') ? Number(u.searchParams.get('count')) : maxPageSize;
 
-	o.results = [];
-		const hash = createHash('sha256');
-		hash.update(String(i));
-		o.results.push({'data':hash.digest('hex'),'id':i});
-	}
+  const o = {};
+  o.request = requestsTotalCount;
+  o.baseIndex = baseIndex;
+  o.startIndex = startIndex;
+  o.pageSize = pageSize;
+  o.totalCount = totalCount;
 
-	const body = JSON.stringify(o);
-	console.log('Response body:  ', body);
-	return body;
+  o.results = [];
+  for (var i = Math.max(baseIndex, startIndex); i < startIndex + pageSize && i < totalCount + baseIndex; i++) {
+    const hash = createHash('sha256');
+    hash.update(String(i));
+    o.results.push({ 'data': hash.digest('hex'), 'id': i });
+  }
+
+  const body = JSON.stringify(o);
+  console.log('Response body:  ', body);
+  return body;
 }
